5ac3ecaee3d54cc1ccaf3f9f088150c217a451f3
Trọng Trọng <hoho3132005@gmail.com>
2025-11-09 00:29:43 +0700

    Refactor Firebase initialization in service worker

--- web-app/public/firebase-messaging-sw.js
+++ web-app/public/firebase-messaging-sw.js
@@ -2,25 +2,27 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
-// Giống hệt config trong firebase.js
-firebase.initializeApp({
-  apiKey: "AIzaSyCl3ozeTqJ2uySRVLoZLC3a7-vn8ffTPeg",
-  authDomain: "matrupro-c3c0c.firebaseapp.com",
-  projectId: "matrupro-c3c0c",
-  storageBucket: "matrupro-c3c0c.firebasestorage.app",
-  messagingSenderId: "1098250533315",
-  appId: "1:1098250533315:web:6ab7bc32da8270e962a576",
-  measurementId: "G-CVEPDTXGJ2"
-});
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Khởi tạo app (compat) cho service worker
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
 // Nhận và hiển thị thông báo khi web đang ở background
 messaging.onBackgroundMessage((payload) => {
-  console.log('📩 [firebase-messaging-sw.js] Received background message ', payload);
-  const { title, body, icon } = payload.notification;
-  self.registration.showNotification(title, {
-    body,
-    icon: icon || '/logo192.png'
-  });
+    console.log(' 📩  [firebase-messaging-sw.js] Received background message ', payload);
+
+    const { title, body, icon } = payload.notification;
+    self.registration.showNotification(title, {
+        body,
+        icon: icon || '/logo192.png'
+    });
 });
